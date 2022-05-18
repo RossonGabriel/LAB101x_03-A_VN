641aa00ecb0e6f56f191e5fd232e9c2bea2dc8a8
Nguyen Tuong Nhat Quang <quang.nguyentuongnhat@gmail.com>
2022-05-18 20:40:14 +0700

    add lab 10 binary search

---
+++ src/lab10BinarySearch/main.java
@@ -0,0 +1,87 @@
+package lab10BinarySearch;
+
+import utils.integerUtils;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static utils.constant.MAX_GENERATED_NUMBER;
+import static utils.constant.MIN_GENERATED_NUMBER;
+
+public class main {
+    public static void main(String[] args) {
+        startProgram();
+    }
+
+    private static void startProgram() {
+        Scanner scanner = new Scanner(System.in);
+
+        //get user length of array
+        int arrayLength = Math.abs(integerUtils.getIntFromUser(scanner, "Enter number of array:", "Please input a true integer"));
+        int[] generatedArray = new int[arrayLength];
+
+        //get user search value
+        int searchValue = integerUtils.getIntFromUser(scanner, "Enter search value:", "Please input a true integer");
+
+        /* generate a random number in array */
+        for (int i = 0; i < arrayLength; i++) {
+            generatedArray[i] = (int)Math.floor(Math.random() * (MAX_GENERATED_NUMBER - MIN_GENERATED_NUMBER) + MIN_GENERATED_NUMBER);
+        }
+
+        quickSort(generatedArray, 0, generatedArray.length - 1);
+        System.out.println("Sorted array: " + Arrays.toString(generatedArray));
+
+        int searchValueIndex = runBinarySearch(generatedArray, searchValue, 0, generatedArray.length - 1);
+        if (searchValueIndex == -1) {
+            System.out.println("Can not found: " + searchValue);
+        } else {
+            System.out.println("Found " + searchValue + " at index: " + searchValueIndex);
+        }
+    }
+
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+
+    private static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+
+    public static int runBinarySearch(int[] sortedArray, int key, int low, int high) {
+        int middle = low  + ((high - low) / 2);
+
+        if (high < low) {
+            return -1;
+        }
+
+        if (key == sortedArray[middle]) {
+            return middle;
+        } else if (key < sortedArray[middle]) {
+            return runBinarySearch(sortedArray, key, low, middle - 1);
+        } else {
+            return runBinarySearch(sortedArray, key, middle + 1, high);
+        }
+    }
+}
--- src/utils/constant.java
+++ src/utils/constant.java
@@ -1,6 +1,6 @@
 package utils;
 
 public class constant {
-    public final static int MAX_GENERATED_NUMBER = 100;
-    public final static int MIN_GENERATED_NUMBER = -100;
+    public final static int MAX_GENERATED_NUMBER = 10;
+    public final static int MIN_GENERATED_NUMBER = -10;
 }
